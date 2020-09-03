0b5b7c5a9aea00b1cf3a29c7eabc62ec02922b6c
Juan D. Salcedo Salazar <jsalcedo@minsa.gob.pe>
2020-09-03 13:07:15 -0500

    [ADD] Módulo de venta de biciletas.

---
+++ bicicleta_store/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name' : 'Tienda de Bicicletas',
+    'version' : '0.0.1',
+    'summary': 'Módulo para vender Bicicletas',
+    'description': """Módulo para administrar la venta
+     venta de Bicicletas""",
+    'category': 'Sale',
+    'website': 'https://www.vendobicicletas.com',
+    'depends' : ['base'],
+    'data': [
+    ],
+    'installable': True,
+}
